b4c4b22ac583445f7d0ea74600aca29e1dbad5f7
HrutikPise <121846857+HrutikPise@users.noreply.github.com>
2023-01-09 16:29:49 +0530

    Delete Desktop directory

--- Desktop/CucumberRestAPIDemos/src/test/java/Resources/GitHubDemo.java
+++
@@ -1,7 +0,0 @@
-package Resources;
-
-public class GitHubDemo {
-	public static void main(String[] args) {
-		System.out.println("Hello world");
-	}
-}
--- Desktop/GitDemoPracties/src/DitDemos/HelloWorldDemo.java
+++
@@ -1,7 +0,0 @@
-package DitDemos;
-
-public class HelloWorldDemo {
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
-}
